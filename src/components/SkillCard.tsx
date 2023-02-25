b94f61a08517df1dff65c35527ced6f516c6e4d0
Amos Boldor <amosboldor@protonmail.com>
2023-02-24 20:25:22 -0800

    style: tweak proficiency tag color

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -3,10 +3,10 @@ import SkillDialog from "./SkillDialog";
 import {useState} from "react";
 
 const proficiencyColors = {
-    'Novice': 'text-orange-100 bg-orange-500',
-    'Intermediate': 'text-yellow-100 bg-yellow-500',
-    'Advanced': 'text-lime-100 bg-lime-500',
-    'Expert': 'text-blue-100 bg-blue-500'
+    'Novice': 'text-zink-800 bg-orange-500',
+    'Intermediate': 'text-zink-600 bg-yellow-300',
+    'Advanced': 'text-zink-700 bg-lime-500',
+    'Expert': 'text-neutral-500 bg-cyan-400'
 }
 
 
@@ -14,12 +14,13 @@ export default function SkillCard({ skill }: { skill: Skill }): JSX.Element {
     let [isOpen, setIsOpen] = useState(false);
     return (
         <li
+            tabIndex={0}
             onClick={() => setIsOpen(true)}
-            className="flex items-center space-x-2 mx-auto p-3 bg-white rounded-xl shadow-lg"
+            className="flex items-center space-x-2 mx-auto m-2 p-2 bg-white rounded-xl shadow-lg cursor-pointer"
         >
-            <img width="30" height="30" src={skill.logo} alt="logo" />
+            <img width="30" src={skill.logo} alt="logo" />
             <div className="inline-flex flex-col">
-                <h3 className="text-md font-medium text-black">{skill.name}</h3>
+                <h3 className="text-md font-medium text-black text-center">{skill.name}</h3>
                 <p className={
                     'mx-auto rounded-md px-1 text-xs min-w-0 ' + proficiencyColors[skill.proficiency]
                 }>{skill.proficiency}</p>
