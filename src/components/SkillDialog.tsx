a3fd624b8bd7bab59cc84503ed7db06a08bf1fe5
Amos Boldor <amosboldor@protonmail.com>
2023-02-24 20:26:24 -0800

    style: make modal look cleaner

--- src/components/SkillDialog.tsx
+++ src/components/SkillDialog.tsx
@@ -13,16 +13,19 @@ export default function SkillDialog(
             className="relative z-50"
         >
             <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-            <div className="fixed inset-0 flex items-center justify-center p-4 max-w-full">
-                <Dialog.Panel className="bg-white shadow overflow-hidden sm:rounded-lg">
-                    <div className="flex justify-between items-center p-1">
-                        <img className="block" width="25" height="25" src={skill.logo} alt="logo" />
+            <div className="fixed inset-0 flex items-start lg:items-center justify-center p-4 max-w-full">
+                <Dialog.Panel className="bg-white shadow overflow-hidden rounded-lg">
+                    <div className="flex justify-end p-2">
                         <button onClick={() => setIsOpen(false)}>
+                            <span className="sr-only">Close panel</span>
                             <XMarkIcon width="25" height="25" />
                         </button>
                     </div>
-                    <div className="px-4 py-5 sm:px-6">
-                        <Dialog.Title className="text-lg leading-6 font-medium text-gray-900">{skill.name}</Dialog.Title>
+                    <div className="px-3 pb-3">
+                        <div className="flex gap-2">
+                            <img className="block" width="25" height="25" src={skill.logo} alt="logo" />
+                            <Dialog.Title className="text-lg font-medium text-gray-900 pb-2">{skill.name}</Dialog.Title>
+                        </div>
                         <Dialog.Description>{skill.description}</Dialog.Description>
                     </div>
                 </Dialog.Panel>
