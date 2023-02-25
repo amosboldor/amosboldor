bef1c769b225a38662cf135c4c4d50a5411f0c6a
Amos Boldor <amosboldor@protonmail.com>
2023-02-24 19:45:32 -0800

    feat: add WCAG skill

---
+++ src/skills/WCAG.tsx
@@ -0,0 +1,10 @@
+import {Skill} from "../models";
+import W3CLogo from "../assets/W3C.svg"
+
+const wcag: Skill = {
+    logo: W3CLogo,
+    name: "WCAG",
+    description: "Web Content Accessibility Guidelines (WCAG)",
+    proficiency: "Novice",
+}
+export default wcag
--- src/skills/index.ts
+++ src/skills/index.ts
@@ -2,10 +2,12 @@ import python from "./Python";
 import {Skill} from "../models";
 import javaScript from "./JavaScript";
 import nodejs from "./NodeJS";
+import wcag from "./WCAG";
 
 const skills: Skill[] = [
     python,
     javaScript,
-    nodejs
+    nodejs,
+    wcag
 ]
 export default skills
